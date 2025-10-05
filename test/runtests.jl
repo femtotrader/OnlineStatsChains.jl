d18bae64ba9a8e61be08af8d0a1695affe75eac1
femtotrader <femto.trader@gmail.com>
2025-10-05 10:44:26 +0200

    feat: enable Rocket.jl integration tests
    - Add [weakdeps] and [extensions] sections to Project.toml
    - Add Rocket dependency to test/Project.toml
    - Update runtests.jl to conditionally run Rocket integration tests
    - Tests now discover and run when Rocket.jl is available
    
    Results: 30/32 Rocket tests passing (2 errors to be fixed separately)

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,19 @@ using TestItemRunner
 
 @run_package_tests
 
+# Conditionally run Rocket.jl integration tests if Rocket is available
+using OnlineStatsChains
+
+# Try to load Rocket to trigger the extension
+try
+    @eval using Rocket
+    # Check if extension loaded
+    if !isnothing(Base.get_extension(OnlineStatsChains, :OnlineStatsChainsRocketExt))
+        @info "Running Rocket.jl integration tests"
+        include("test_rocket_integration.jl")
+    else
+        @warn "Rocket.jl is installed but extension did not load"
+    end
+catch e
+    @info "Rocket.jl not available, skipping integration tests: $e"
+end
